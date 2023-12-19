d087220016cab528cb28d13323cb1242b99426cf
Даниил <daniilmysyk15@gmail.com>
2023-12-20 02:54:07 +0300

    good

--- edit_project.py
+++ edit_project.py
@@ -8,58 +8,11 @@ class EditProject(QtWidgets.QWidget):
         self.db = db
         self.main_app = main_app
         self.project_id = project_id
+        self.project = self.db.get_project(self.project_id)
         self.init_ui()
 
-    # def init_ui(self):
-    #     self.project = self.db.get_project(self.project_id)
-    #
-    #     if self.project is not None:
-    #         self.name_label = QtWidgets.QLabel('Название проекта:')
-    #         self.name_input = QtWidgets.QLineEdit(self.project[1])
-    #         self.description_label = QtWidgets.QLabel('Описание проекта:')
-    #         self.description_input = QtWidgets.QTextEdit(self.project[2])
-    #         self.manager_label = QtWidgets.QLabel('ID менеджера:')
-    #         self.manager_input = QtWidgets.QLineEdit(str(self.project[3]))
-    #         self.start_date_label = QtWidgets.QLabel('Дата начала:')
-    #         self.start_date_input = QtWidgets.QLineEdit(self.project[4])
-    #         self.end_date_label = QtWidgets.QLabel('Дата окончания:')
-    #         self.end_date_input = QtWidgets.QLineEdit(self.project[5])
-    #         self.update_button = QtWidgets.QPushButton('Обновить проект')
-    #
-    #         layout = QtWidgets.QVBoxLayout()
-    #         layout.addWidget(self.name_label)
-    #         layout.addWidget(self.name_input)
-    #         layout.addWidget(self.description_label)
-    #         layout.addWidget(self.description_input)
-    #         layout.addWidget(self.manager_label)
-    #         layout.addWidget(self.manager_input)
-    #         layout.addWidget(self.start_date_label)
-    #         layout.addWidget(self.start_date_input)
-    #         layout.addWidget(self.end_date_label)
-    #         layout.addWidget(self.end_date_input)
-    #         layout.addWidget(self.update_button)
-    #
-    #         self.setLayout(layout)
-    #
-    #         self.apply_button = QtWidgets.QPushButton('Применить')
-    #         self.back_button = QtWidgets.QPushButton('Назад')
-    #
-    #         layout.addWidget(self.apply_button)
-    #         layout.addWidget(self.back_button)
-    #
-    #         self.setLayout(layout)
-    #
-    #         self.apply_button.clicked.connect(self.update_project)
-    #         self.back_button.clicked.connect(self.main_app.switch_to_main_menu)
-    #
-    #         self.update_button.clicked.connect(self.update_project)
-    #     else:
-    #         print(f"Проект с ID {self.project_id} не найден в базе данных.")
-
     def init_ui(self):
-        self.project = self.db.get_project(self.project_id)
-
-        if self.project is not None:
+        if self.project:
             self.name_input = QtWidgets.QLineEdit(self.project[1])
             self.description_input = QtWidgets.QTextEdit(self.project[2])
             self.manager_input = QtWidgets.QLineEdit(str(self.project[3]))
@@ -79,8 +32,10 @@ class EditProject(QtWidgets.QWidget):
 
             self.update_button.clicked.connect(self.update_project)
         else:
-            print(f"Проект с ID {self.project_id} не найден в базе данных.")
-
+            self.error_label = QtWidgets.QLabel(f"Проект с ID {self.project_id} не найден")
+            layout = QtWidgets.QVBoxLayout()
+            layout.addWidget(self.error_label)
+            self.setLayout(layout)
 
     def update_project(self):
         name = self.name_input.text()
@@ -90,16 +45,4 @@ class EditProject(QtWidgets.QWidget):
         end_date = self.end_date_input.text()
         self.db.update_project(self.project_id, name, description, manager_id, start_date, end_date)
         print('Проект успешно обновлен')
-
-
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    # Замените следующую строку на фактический project_id, который вы хотите отредактировать
-    project_id = 1
-    edit_project = EditProject(project_id)
-    edit_project.show()
-    sys.exit(app.exec())
-
-
-if __name__ == "__main__":
-    main()
+        self.main_app.switch_to_main_menu()
--- main_menu.py
+++ main_menu.py
@@ -1,6 +1,5 @@
-import sys
-
 from PyQt6 import QtWidgets
+from project_selection import ProjectSelection  # Импортируйте новый класс
 
 class MainMenu(QtWidgets.QWidget):
     def __init__(self, db, main_app):
@@ -9,15 +8,11 @@ class MainMenu(QtWidgets.QWidget):
         self.main_app = main_app
         self.init_ui()
 
-
-
     def init_ui(self):
-        # self.projects_list.itemClicked.connect(self.item_clicked)
         self.new_project_button = QtWidgets.QPushButton('Создать новый проект')
         self.view_projects_button = QtWidgets.QPushButton('Просмотреть проекты')
         self.edit_project_button = QtWidgets.QPushButton('Редактировать проект')
         self.delete_project_button = QtWidgets.QPushButton('Удалить проект')
-
 
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(self.new_project_button)
@@ -25,20 +20,14 @@ class MainMenu(QtWidgets.QWidget):
         layout.addWidget(self.edit_project_button)
         layout.addWidget(self.delete_project_button)
 
-
         self.setLayout(layout)
 
         self.new_project_button.clicked.connect(self.main_app.switch_to_create_project)
         self.view_projects_button.clicked.connect(self.main_app.switch_to_view_projects)
-        self.edit_project_button.clicked.connect(self.main_app.switch_to_edit_project)
+        self.edit_project_button.clicked.connect(self.switch_to_project_selection)
         self.delete_project_button.clicked.connect(self.main_app.switch_to_delete_project)
 
-
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    main_menu = MainMenu()
-    main_menu.show()
-    sys.exit(app.exec())
-
-if __name__ == "__main__":
-    main()
+    def switch_to_project_selection(self):
+        self.hide()
+        self.project_selection = ProjectSelection(self.db, self)
+        self.project_selection.show()
